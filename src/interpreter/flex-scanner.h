8582a66837a097170b8258eb7e711f90d4fbb9bc
Florian Galinier <galinierflo@gmail.com>
2015-05-23 21:49:30 +0200

    Analyseurs : Ajout de la doc doxygen

--- src/interpreter/flex-scanner.h
+++ src/interpreter/flex-scanner.h
@@ -1,3 +1,14 @@
+/**
+ * \file flex-scanner.h
+ * \brief FlexScanner class header.
+ * \author Florian Galinier
+ * \version 1.0
+ * \date 26/04/15
+ *
+ * Definition of the FlexScanner class inheriting yyFlexLexer.
+ *
+ */
+
 #pragma once
 
 #undef YY_DECL
@@ -9,11 +20,29 @@
 
 #include "y.tab.h"
 
-
 namespace stibbons {
+	/**
+	  * \class FlexScanner
+	  * \brief Class for lexical analysis.
+	  *
+	  * This class contain an overload of yylex function with parameters used
+	  * by our lexical parser.
+	  *
+	  * \author Florian Galinier
+	  */
 	class FlexScanner : public yyFlexLexer {
 	public:
+		/**
+		 * FlexScanner constructor.
+		 * \param in The stream that will be parsed by yylex() method.
+		 */
 		FlexScanner(std::istream* in) : yyFlexLexer(in) {}
+		/**
+		 * Return the next token.
+		 * \param[out] pyylval The value of the token.
+		 * \param[out] loc Location of the token.
+		 * \return The last yy::parser::token found or 0 if end of stream.
+		 */
 		int yylex(yy::parser::semantic_type* pyylval, yy::parser::location_type* loc) { 
 			this->pyylval = pyylval; 
 			this->loc = loc; 
@@ -21,6 +50,11 @@ namespace stibbons {
 		}
 		
 	private:
+		/**
+		 * Inherited yylex method.
+		 * This method is called by public overloaded yylex method.
+		 * \return The last yy::parser::token found or 0 if end of stream.
+		 */
 		int yylex();
 		yy::parser::semantic_type* pyylval;
 		yy::parser::location_type* loc;
